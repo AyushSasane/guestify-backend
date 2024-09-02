a325225e29300be8c06b3e0250a60802c15632d5
AyushSasane <ayushsasane@gmail.com>
2024-09-02 20:03:51 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Root directory where index.html is located
-  build: {
-    outDir: 'dist', // Output directory
-  },
 })
